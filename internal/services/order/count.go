c8ea22a0247497b6369f82c88b585dad6d89d9fa
wangben <wangben-1203@qq.com>
2025-05-24 18:11:10 +0800

    count

--- internal/services/order/count.go
+++ internal/services/order/count.go
@@ -2,16 +2,26 @@ package order
 
 import (
 	"context"
-	"time"
-
 	"example/internal/repositories/order"
 )
 
-func (o *Order) Count(ctx context.Context) (int64, error) {
+type Count struct {
+	ID      *int
+	OrderNo *string
+}
+
+func (o *Order) Count(ctx context.Context, sharding string, data *Count) (int64, error) {
+	conditions := make([]order.ConditionOption, 0)
+	conditions = append(conditions, order.ConditionShardingEq(sharding))
+	conditions = append(conditions, order.ConditionDeletedAtIsZero())
+	if data.ID != nil {
+		conditions = append(conditions, order.ConditionID(*data.ID))
+	}
+	if data.OrderNo != nil {
+		conditions = append(conditions, order.ConditionOrderNoEq(*data.OrderNo))
+	}
 	return o.orderRepo.
 		Count().
-		Where(
-			order.ConditionShardingEq(time.Now().Format("200601")),
-			order.ConditionDeletedAtIsZero(),
-		).Do(ctx)
+		Where(conditions...).
+		Do(ctx)
 }
--- internal/services/order/count_test.go
+++ internal/services/order/count_test.go
@@ -3,10 +3,13 @@ package order_test
 import (
 	"context"
 	"testing"
+	"time"
+
+	"example/internal/services/order"
 )
 
 func TestCount(t *testing.T) {
-	count, err := orderSvc.Count(context.Background())
+	count, err := orderSvc.Count(context.Background(), time.Now().Format("200601"), &order.Count{})
 	if err != nil {
 		t.Fatal(err)
 		return
