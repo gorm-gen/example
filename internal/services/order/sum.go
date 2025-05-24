57efefee4dd3ba0fab3d2416d4c8933ef694f383
wangben <wangben-1203@qq.com>
2025-05-24 18:14:16 +0800

    sum

--- internal/services/order/sum.go
+++ internal/services/order/sum.go
@@ -2,18 +2,24 @@ package order
 
 import (
 	"context"
-	"time"
 
 	"github.com/shopspring/decimal"
 
 	"example/internal/repositories/order"
 )
 
-func (o *Order) Sum(ctx context.Context) (decimal.Decimal, error) {
-	return o.orderRepo.
-		Sum(o.q.Order.Amount).
-		Where(
-			order.ConditionShardingEq(time.Now().Format("200601")),
-			order.ConditionDeletedAtIsZero(),
-		).Do(ctx)
+type Sum struct {
+	UID *int
+}
+
+func (o *Order) Sum(ctx context.Context, sharding string, data *Sum) (decimal.Decimal, error) {
+	conditions := make([]order.ConditionOption, 0)
+	conditions = append(conditions, order.ConditionShardingEq(sharding))
+	conditions = append(conditions, order.ConditionDeletedAtIsZero())
+	if data.UID != nil {
+		conditions = append(conditions, order.ConditionUID(*data.UID))
+	}
+	return o.orderRepo.Sum(o.q.Order.Amount).
+		Where(conditions...).
+		Do(ctx)
 }
--- internal/services/order/sum_test.go
+++ internal/services/order/sum_test.go
@@ -3,10 +3,13 @@ package order_test
 import (
 	"context"
 	"testing"
+	"time"
+
+	"example/internal/services/order"
 )
 
 func TestSum(t *testing.T) {
-	sum, err := orderSvc.Sum(context.Background())
+	sum, err := orderSvc.Sum(context.Background(), time.Now().Format("200601"), &order.Sum{})
 	if err != nil {
 		t.Fatal(err)
 		return
