aec704aa40e69a46f6a0fbca20b35d06880cd448
wangben <wangben-1203@qq.com>
2025-05-24 18:33:44 +0800

    last

---
+++ internal/services/order/last_test.go
@@ -0,0 +1,24 @@
+package order_test
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"example/internal/services/order"
+)
+
+func TestLast(t *testing.T) {
+	res, err := orderSvc.Last(context.Background(), time.Now().Format("200601"), &order.Last{})
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	var bytes []byte
+	if bytes, err = json.Marshal(res); err != nil {
+		t.Fatal(err)
+		return
+	}
+	t.Log(string(bytes))
+}
