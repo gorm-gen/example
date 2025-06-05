d63fbe4651fe0757d12650c9ee67c3826ee9b443
wangben <wangben-1203@qq.com>
2025-06-05 16:01:00 +0800

    repo

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/gorm-gen/plugin/generate"
 	"github.com/gorm-gen/repository"
+	"gorm.io/gen"
+	"gorm.io/gen/field"
 
 	"example/internal/global"
 	"example/internal/initialize/config"
@@ -41,15 +43,117 @@ func main() {
 	}
 
 	g := generate.New(global.DB, opts...)
+	generator := g.Generator()
 
 	// 1、生成models
 	g.SetGenerateModel("order")
 	g.SetGenerateModel("order_item")
+	company := generator.GenerateModel("company")
+	identityCard := generator.GenerateModel("identity_card")
+	creditCard := generator.GenerateModel("credit_card")
+	language := generator.GenerateModel("language")
+	classify := generator.GenerateModel("classify")
+	area := generator.GenerateModel("area")
+	userLanguage := generator.GenerateModel("user_language")
+
+	user := generator.GenerateModel("user",
+		gen.FieldRelate(field.BelongsTo, "Company", company, &field.RelateConfig{
+			RelatePointer: true,
+			JSONTag:       "company,omitempty",
+			GORMTag: field.GormTag{
+				"foreignKey": {"CompanyID"},
+				"references": {"ID"},
+			},
+		}),
+		gen.FieldRelate(field.HasOne, "IdentityCard", identityCard, &field.RelateConfig{
+			RelatePointer: true,
+			JSONTag:       "identity_card,omitempty",
+			GORMTag: field.GormTag{
+				"foreignKey": {"UserID"},
+				"references": {"ID"},
+			},
+		}),
+		gen.FieldRelate(field.HasMany, "CreditCards", creditCard, &field.RelateConfig{
+			RelateSlicePointer: true,
+			JSONTag:            "credit_cards,omitempty",
+			GORMTag: field.GormTag{
+				"foreignKey": {"UserID"},
+				"references": {"ID"},
+			},
+		}),
+		gen.FieldRelate(field.Many2Many, "Languages", language, &field.RelateConfig{
+			JSONTag: "languages,omitempty",
+			GORMTag: field.GormTag{
+				"many2many":      {"user_language"},
+				"foreignKey":     {"ID"},
+				"joinForeignKey": {"UserID"},
+				"references":     {"ID"},
+				"joinReferences": {"LanguageID"},
+			},
+		}),
+	)
+
+	languages := generator.GenerateModel("language",
+		gen.FieldRelate(field.Many2Many, "Users", user, &field.RelateConfig{
+			JSONTag: "users,omitempty",
+			GORMTag: field.GormTag{
+				"many2many":      {"user_language"},
+				"foreignKey":     {"ID"},
+				"joinForeignKey": {"LanguageID"},
+				"references":     {"ID"},
+				"joinReferences": {"UserID"},
+			},
+		}),
+	)
+
+	classifies := generator.GenerateModel("classify",
+		gen.FieldRelate(field.BelongsTo, "Parent", classify, &field.RelateConfig{
+			RelatePointer: true,
+			JSONTag:       "parent,omitempty",
+			GORMTag: field.GormTag{
+				"foreignKey": {"Pid"},
+				"references": {"ID"},
+			},
+		}),
+		gen.FieldRelate(field.HasMany, "Child", classify, &field.RelateConfig{
+			JSONTag: "child,omitempty",
+			GORMTag: field.GormTag{
+				"foreignKey": {"Pid"},
+				"references": {"ID"},
+			},
+		}),
+	)
+
+	areas := generator.GenerateModel("area",
+		gen.FieldRelate(field.HasOne, "Parent", area, &field.RelateConfig{
+			RelatePointer: true,
+			JSONTag:       "parent,omitempty",
+			GORMTag: field.GormTag{
+				"foreignKey": {"Pid"},
+				"references": {"ID"},
+			},
+		}),
+		gen.FieldRelate(field.HasMany, "Child", area, &field.RelateConfig{
+			JSONTag: "child,omitempty",
+			GORMTag: field.GormTag{
+				"foreignKey": {"Pid"},
+				"references": {"ID"},
+			},
+		}),
+	)
 
 	// 2、生成gen.query
 	g.SetApplyBasic(
 		models.Order{},
 		models.OrderItem{},
+		company,
+		user,
+		identityCard,
+		creditCard,
+		languages,
+		classifies,
+		areas,
+		userLanguage,
 	)
 
 	g.Execute()
@@ -68,6 +172,14 @@ func main() {
 	if err := r.Generate(
 		models.Order{},
 		models.OrderItem{},
+		models.User{},
+		models.Company{},
+		models.IdentityCard{},
+		models.CreditCard{},
+		models.Language{},
+		models.Classify{},
+		models.Area{},
+		models.UserLanguage{},
 	); err != nil {
 		log.Fatal(err)
 		return
--- internal/query/gen.go
+++ internal/query/gen.go
@@ -16,39 +16,79 @@ import (
 )
 
 var (
-	Q         = new(Query)
-	Order     *order
-	OrderItem *orderItem
+	Q            = new(Query)
+	Area         *area
+	Classify     *classify
+	Company      *company
+	CreditCard   *creditCard
+	IdentityCard *identityCard
+	Language     *language
+	Order        *order
+	OrderItem    *orderItem
+	User         *user
+	UserLanguage *userLanguage
 )
 
 func SetDefault(db *gorm.DB, opts ...gen.DOOption) {
 	*Q = *Use(db, opts...)
+	Area = &Q.Area
+	Classify = &Q.Classify
+	Company = &Q.Company
+	CreditCard = &Q.CreditCard
+	IdentityCard = &Q.IdentityCard
+	Language = &Q.Language
 	Order = &Q.Order
 	OrderItem = &Q.OrderItem
+	User = &Q.User
+	UserLanguage = &Q.UserLanguage
 }
 
 func Use(db *gorm.DB, opts ...gen.DOOption) *Query {
 	return &Query{
-		db:        db,
-		Order:     newOrder(db, opts...),
-		OrderItem: newOrderItem(db, opts...),
+		db:           db,
+		Area:         newArea(db, opts...),
+		Classify:     newClassify(db, opts...),
+		Company:      newCompany(db, opts...),
+		CreditCard:   newCreditCard(db, opts...),
+		IdentityCard: newIdentityCard(db, opts...),
+		Language:     newLanguage(db, opts...),
+		Order:        newOrder(db, opts...),
+		OrderItem:    newOrderItem(db, opts...),
+		User:         newUser(db, opts...),
+		UserLanguage: newUserLanguage(db, opts...),
 	}
 }
 
 type Query struct {
 	db *gorm.DB
 
-	Order     order
-	OrderItem orderItem
+	Area         area
+	Classify     classify
+	Company      company
+	CreditCard   creditCard
+	IdentityCard identityCard
+	Language     language
+	Order        order
+	OrderItem    orderItem
+	User         user
+	UserLanguage userLanguage
 }
 
 func (q *Query) Available() bool { return q.db != nil }
 
 func (q *Query) clone(db *gorm.DB) *Query {
 	return &Query{
-		db:        db,
-		Order:     q.Order.clone(db),
-		OrderItem: q.OrderItem.clone(db),
+		db:           db,
+		Area:         q.Area.clone(db),
+		Classify:     q.Classify.clone(db),
+		Company:      q.Company.clone(db),
+		CreditCard:   q.CreditCard.clone(db),
+		IdentityCard: q.IdentityCard.clone(db),
+		Language:     q.Language.clone(db),
+		Order:        q.Order.clone(db),
+		OrderItem:    q.OrderItem.clone(db),
+		User:         q.User.clone(db),
+		UserLanguage: q.UserLanguage.clone(db),
 	}
 }
 
@@ -62,21 +102,45 @@ func (q *Query) WriteDB() *Query {
 
 func (q *Query) ReplaceDB(db *gorm.DB) *Query {
 	return &Query{
-		db:        db,
-		Order:     q.Order.replaceDB(db),
-		OrderItem: q.OrderItem.replaceDB(db),
+		db:           db,
+		Area:         q.Area.replaceDB(db),
+		Classify:     q.Classify.replaceDB(db),
+		Company:      q.Company.replaceDB(db),
+		CreditCard:   q.CreditCard.replaceDB(db),
+		IdentityCard: q.IdentityCard.replaceDB(db),
+		Language:     q.Language.replaceDB(db),
+		Order:        q.Order.replaceDB(db),
+		OrderItem:    q.OrderItem.replaceDB(db),
+		User:         q.User.replaceDB(db),
+		UserLanguage: q.UserLanguage.replaceDB(db),
 	}
 }
 
 type queryCtx struct {
-	Order     IOrderDo
-	OrderItem IOrderItemDo
+	Area         IAreaDo
+	Classify     IClassifyDo
+	Company      ICompanyDo
+	CreditCard   ICreditCardDo
+	IdentityCard IIdentityCardDo
+	Language     ILanguageDo
+	Order        IOrderDo
+	OrderItem    IOrderItemDo
+	User         IUserDo
+	UserLanguage IUserLanguageDo
 }
 
 func (q *Query) WithContext(ctx context.Context) *queryCtx {
 	return &queryCtx{
-		Order:     q.Order.WithContext(ctx),
-		OrderItem: q.OrderItem.WithContext(ctx),
+		Area:         q.Area.WithContext(ctx),
+		Classify:     q.Classify.WithContext(ctx),
+		Company:      q.Company.WithContext(ctx),
+		CreditCard:   q.CreditCard.WithContext(ctx),
+		IdentityCard: q.IdentityCard.WithContext(ctx),
+		Language:     q.Language.WithContext(ctx),
+		Order:        q.Order.WithContext(ctx),
+		OrderItem:    q.OrderItem.WithContext(ctx),
+		User:         q.User.WithContext(ctx),
+		UserLanguage: q.UserLanguage.WithContext(ctx),
 	}
 }
 
