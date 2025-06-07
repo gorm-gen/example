e383c6881b4ff0d2e9f379ab4b12f3ccc8ab2d7c
wangben <wangben-1203@qq.com>
2025-06-07 11:03:18 +0800

    m

--- internal/query/methods/method.go
+++ internal/query/methods/method.go
@@ -1,1 +1,17 @@
 package methods
+
+import (
+	"gorm.io/gen"
+)
+
+type Query interface {
+	// GetByID
+	// SELECT * FROM @@table WHERE `id` = @id AND `deleted_at` = 0
+	GetByID(id int) (gen.T, error)
+}
+
+type User interface {
+	// GetCompanyName
+	// SELECT `name` FROM `company` WHERE `id` = (SELECT `id` FROM @@table WHERE `id` = @id AND `deleted_at` = 0) AND `deleted_at` = 0
+	GetCompanyName(id int) (gen.M, error)
+}
